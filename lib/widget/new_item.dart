7f725566afacb1253642b4f89ae485bd4ee57e0d
Sandeep Kumar M <sdagangstar11@gmail.com>
2023-08-02 12:28:58 +0530

    Learn to setup FireBase for backend of flutter applications, Use HTTP post method to add new item to firebase real-time database

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
@@ -21,6 +24,17 @@ class _NewItemState extends State<NewItem> {
   void saveItem() {
     if (formKey.currentState!.validate()) {
       formKey.currentState!.save();
+      final url = Uri.https("flutter-test-63344-default-rtdb.firebaseio.com",
+          "shopping-List.json");
+      http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({
+          "name": enteredName,
+          "quantity": enteredQuantity,
+          "category": selectedCategory.category,
+        }),
+      );
       Navigator.of(context).pop(GroceryItem(
         id: DateTime.now().toString(),
         name: enteredName,
